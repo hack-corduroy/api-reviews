0e32ad24d54c64a11da6294ff7a2d59921ffb06a
Megan Wolf <meganwolf25@gmail.com>
2022-01-12 16:18:56 -0500

    Data imported

--- database/Schema.sql
+++ database/Schema.sql
@@ -1,51 +1,58 @@
-DROP TABLE IF EXISTS reviews, review_info, metadata, ratings, characteristics  CASCADE
+DROP TABLE IF EXISTS characteristic_reviews, reviews, reviews_photos, characteristics;
 
--- psql -d reviews -f database/SCHEMA.sql
+-- psql -d reviews -f database/Schema.sql
+
+-- Characteristic Reviews --
+
+CREATE TABLE IF NOT EXISTS characteristic_reviews (
+  id INTEGER PRIMARY KEY,
+  characteristic_id INTEGER,
+  review_id INTEGER,
+  value INTEGER
+);
+
+COPY characteristic_reviews FROM '/Users/meganwolf/HackReactor/SDC Project/api-reviews/csv/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+CREATE INDEX review_idx on characteristic_reviews (review_id);
 
 -- Reviews --
 
 CREATE TABLE IF NOT EXISTS reviews (
   id INTEGER PRIMARY KEY,
   product_id INTEGER,
-  review_id INTEGER
+  rating INTEGER,
+  review_date DATE,
+  summary VARCHAR,
+  body VARCHAR,
+  recommend BOOLEAN,
+  reported BOOLEAN,
+  reviewer_name VARCHAR(50),
+  reviewer_email VARCHAR(50),
+  response VARCHAR,
+  helpfulness INTEGER
 );
 
--- Review Info --
+COPY reviews FROM '/Users/meganwolf/HackReactor/SDC Project/api-reviews/csv/reviews.csv' DELIMITER ',' CSV HEADER;
+CREATE INDEX product_idx on reviews (product_id);
 
-CREATE TABLE IF NOT EXISTS review_info (
+-- Review Photos --
+
+CREATE TABLE IF NOT EXISTS reviews_photos (
   id INTEGER PRIMARY KEY,
   review_id INTEGER,
-  rating INTEGER,
-  summary TEXT,
-  recommend BOOLEAN,
-  response TEXT,
-  body TEXT,
-  review_date DATE,
-  reviewer_name TEXT,
-  helpfulness INTEGER,
-  photos VARCHAR(400)
+  url VARCHAR
 );
 
--- Metadata --
-
-CREATE TABLE IF NOT EXISTS metadata (
-  id INTEGER PRIMARY KEY,
-  product_id INTEGER,
-  ratings INTEGER,
-  recommend INTEGER,
-  characteristics VARCHAR(250)
-);
-
--- Ratings --
-
-CREATE TABLE IF NOT EXISTS ratings (
-  id INTEGER PRIMARY KEY,
-  star_rating INTEGER
-);
+COPY reviews_photos FROM '/Users/meganwolf/HackReactor/SDC Project/api-reviews/csv/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+CREATE INDEX reviewPhoto_idx on reviews_photos (review_id);
 
 -- Characteristics --
 
 CREATE TABLE IF NOT EXISTS characteristics (
   id INTEGER PRIMARY KEY,
-  characteristics TEXT
+  product_id INTEGER,
+  name VARCHAR(50)
 );
+
+COPY characteristics FROM '/Users/meganwolf/HackReactor/SDC Project/api-reviews/csv/characteristics.csv' DELIMITER ',' CSV HEADER;
+CREATE INDEX productChar_idx on characteristics (product_id);
+

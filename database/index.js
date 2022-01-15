d766fdb4efca6df13909d95071199dd57c40c87c
Megan Wolf <meganwolf25@gmail.com>
2022-01-15 10:05:41 -0500

    API Endpoint Working

--- database/index.js
+++ database/index.js
@@ -5,4 +5,34 @@ const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_P
 
 const pool = new Pool({ connectionString: connectionString });
 
-module.exports = pool;
+const db = {
+  getReviews: async (page = 1, limit = 50, id = 2) => {
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    const productId = id
+    const sql = `
+    SELECT reviews.id AS review_id,
+    reviews.rating, reviews.summary, reviews.recommend, reviews.response,
+    reviews.body, reviews.review_date as date,
+    reviews.reviewer_name, reviews.helpfulness,
+    json_agg(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url)) AS photos
+    FROM reviews LEFT JOIN reviews_photos ON reviews_photos.review_id = reviews.id
+    WHERE product_id= ${productId} AND reviews.reported=false AND reviews.id > ${start} AND reviews.id <= ${end} GROUP BY reviews.id
+    `;
+    let data = await pool.query(sql);
+    return data.rows;
+  }
+
+  // getMeta: async (id) => {
+  //   const sql = `
+  //   SELECT *
+  //   FROM reviews
+  //   WHERE product_id = ${id} AND id > ${start} AND id <= ${end}
+  //   order by helpfulness
+  //   `;
+  //   let data = await pool.query(sql);
+  //   return data.rows;
+  // }
+}
+
+module.exports = { pool, db };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const { db } = require('../database/index.js');
 
 app.get('/hello', (req, res) => {
   res.json({ hello: 'world' });
@@ -9,3 +10,20 @@ app.get('/hello', (req, res) => {
 app.listen(PORT, () => {
   console.log('Server is running at http://localhost:' + PORT);
 });
+
+app.get('/reviews', async (req, res) => {
+  let result = await db.getReviews();
+  res.status(200).send(result);
+});
+
+// app.get('/reviews/meta', (req, res) => {
+//   db.getMeta(req.query, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       res.sendStatus(404);
+//     } else {
+//       res.status(200).send(result);
+//     }
+//   });
+// });
+
